b654dfa63dda20d5c186283d889d3149720c98f4
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2025-01-29 11:28:16 +0100

    #2 get sum

--- 01-basic-challenges-1/02-get-sum/get-sum.test.js
+++ 01-basic-challenges-1/02-get-sum/get-sum.test.js
@@ -2,12 +2,12 @@ const getSum = require('./get-sum');
 
 test('Calculating the sum of two numbers', () => {
   // Test case inputs
-  const num1 = 5;
-  const num2 = 7;
+  const num1 = 10;
+  const num2 = 10;
 
   // Call the function
   const result = getSum(num1, num2);
 
   // Check if the result is equal to the expected sum
-  expect(result).toBe(12);
+  expect(result).toBe(20);
 });
