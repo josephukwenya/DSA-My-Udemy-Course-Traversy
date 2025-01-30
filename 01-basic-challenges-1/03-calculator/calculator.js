7a27fdca12a84bf85942e2b9442ca1fca013c933
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2025-01-30 09:51:07 +0100

    #3 solution 3

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,60 @@
-function calculator() {}
+// Solution 1 - Using switch statement
+// function calculator(num1, num2, operator) {
+//   let result;
+
+//   switch (operator) {
+//     case '+':
+//       result = num1 + num2;
+//       break;
+//     case '-':
+//       result = num1 - num2;
+//       break;
+//     case '*':
+//       result = num1 * num2;
+//       break;
+//     case '/':
+//       result = num1 / num2;
+//       break;
+//     default:
+//       result = 'Invalid operator';
+//   }
+
+//   return result;
+// }
+
+// Solution 2 - Using if-else conditional statement
+// function calculator(num1, num2, operator) {
+//   let result;
+
+//   if (operator === '+') {
+//     result = num1 + num2;
+//   } else if (operator === '-') {
+//     result = num1 - num2;
+//   } else if (operator === '*') {
+//     result = num1 * num2;
+//   } else if (operator === '/') {
+//     result = num1 / num2;
+//   } else {
+//     result = 'Invalid operator';
+//   }
+
+//   return result;
+// }
+
+// Solution 3 - Using object lookup
+function calculator(num1, num2, operator) {
+  const operations = {
+    '+': num1 + num2,
+    '-': num1 - num2,
+    '*': num1 * num2,
+    '/': num2 !== 0 ? num1 / num2 : 'Cannot divide by zero',
+  };
+  return operations[operator] || 'Invalid operator';
+}
+
+// Solution 4 - Using functional programming with reduce
+function calculator4(num1, num2, operator) {}
+
+// Functional programming with reduce
 
 module.exports = calculator;
