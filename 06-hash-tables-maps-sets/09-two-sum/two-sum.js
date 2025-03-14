c86465b009fcc55167c3201652c2be9acf1bd87d
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2025-03-14 10:46:18 +0100

    two sum solved

--- 06-hash-tables-maps-sets/09-two-sum/two-sum.js
+++ 06-hash-tables-maps-sets/09-two-sum/two-sum.js
@@ -1,3 +1,19 @@
-function twoSum() {}
+function twoSum(nums, target) {
+  const newSet = new Set();
+
+  for (let i = 0; i < nums.length; i++) {
+    const compliment = target - nums[i];
+
+    if (newSet.has(compliment)) {
+      return [nums.indexOf(compliment), i];
+    }
+
+    newSet.add(nums[i]);
+  }
+
+  return [];
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
 
 module.exports = twoSum;
