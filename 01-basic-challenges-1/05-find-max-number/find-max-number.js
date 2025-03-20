c50aa7a302f86b84d7f073560bb28e194b9c694d
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2025-03-20 14:34:19 +0100

    #5 find max solution

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,17 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  let count = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > count) {
+      count = arr[i];
+    }
+  }
+
+  return count;
+}
+
+// const findMaxNumber = (arr) => {
+//   return Math.max(...arr);
+// };
 
 module.exports = findMaxNumber;
